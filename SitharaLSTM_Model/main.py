e2ef4f328577e29d7dbe2b00f38028c0e526eeed
Adityakrishnan <adityakrishnanp007@gmail.com>
2021-05-18 03:03:32 +0530

    Secondary Commit

--- SitharaLSTM_Model/main.py
+++ SitharaLSTM_Model/main.py
@@ -1,4 +1,19 @@
-import tensorflow
-from tensorflow import keras
-import numpy
+import tensorflow as tf
 
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+
+import numpy as np
+
+tokenizer = Tokenizer()
+data = open('').read()
+
+corpus = data.lower().split("\n")
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+print(tokenizer.word_index)
+print(total_words)
