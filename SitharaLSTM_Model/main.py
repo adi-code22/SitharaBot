39d4aca636aa41a97158fd05cae8b84273e3033a
cloneartist <73981868+cloneartist@users.noreply.github.com>
2021-05-19 14:35:33 +0530

    Add import

--- SitharaLSTM_Model/main.py
+++ SitharaLSTM_Model/main.py
@@ -1,12 +1,13 @@
 import tensorflow as tf
 
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional,Dropout
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-
+from tensorflow import keras
 import numpy as np
+
 
 tokenizer = Tokenizer()
 data = open('').read()
