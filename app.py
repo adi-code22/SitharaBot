8dad7288bab18e3a71dbd988539f57e1e6fbedb7
cloneartist <73981868+cloneartist@users.noreply.github.com>
2021-05-24 17:26:32 +0530

    Add predict function to connect ML model to frontend

--- app.py
+++ app.py
@@ -9,8 +9,58 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    my_prediction = 1
-    return render_template('result.html',prediction = my_prediction)
+    import tensorflow as tf
+
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.models import Sequential
+    from tensorflow.keras.optimizers import Adam
+    from tensorflow import keras
+    import numpy as np
+    from keras.models import model_from_json
+    data = open('.\\Dataset1-Sithara.txt').read()
+    # open('enter location of dataset')
+    # print(data)
+
+    corpus = data.lower().split("\n")
+    corpus = list(set(corpus))
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(corpus)
+    total_words = len(tokenizer.word_index) + 1
+    # print(tokenizer.word_index)
+    input_sequences = []
+    for line in corpus:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+
+        for i in range(1, len(token_list)):
+            n_gram_sequence = token_list[:i + 1]
+            input_sequences.append(n_gram_sequence)
+        # print(n_gram_sequence)
+    max_sequence_len = max([len(x) for x in input_sequences])
+
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights('model.h5')
+    model = loaded_model
+    if request.method == 'POST':
+        seed_text = request.form['message']
+        next_words = 10
+
+        for _ in range(next_words):
+            token_list = tokenizer.texts_to_sequences([seed_text])[0]
+            token_list = pad_sequences([token_list], maxlen=max_sequence_len - 1, padding='pre')
+            predicted = model.predict_classes(token_list, verbose=0)
+            output_word = ""
+            for word, index in tokenizer.word_index.items():
+                if index == predicted:
+                    output_word = word
+                    break
+            seed_text += " " + output_word
+        my_prediction=seed_text
+        return render_template('result.html',prediction = my_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
